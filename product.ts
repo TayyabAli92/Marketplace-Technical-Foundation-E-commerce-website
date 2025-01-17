314201961977bce1433a77dbe7b1165ef8d69ac7
Tayyab Ali <hameshak110@gmail.com>
2025-01-17 11:12:46 +0500

    first commit

---
+++ product.ts
@@ -0,0 +1,13 @@
+export default {
+    name: 'product',
+    type: 'document',
+    fields: [
+    { name: 'id', type: 'number', title: 'unique indentifier' },
+    { name: 'name', type: 'string', title: 'Product Name' },
+    { name: 'description', type: 'text', title: 'Product Description' },
+    { name: 'title', type: 'string', title: 'Product Title' },
+    { name: 'price', type: 'number', title: 'Price' },
+    ]
+   }; 
+
+   
